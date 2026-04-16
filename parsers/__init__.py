d45010f8e3a2443401eab8e608e9751de8a5d424
Fillbob <romanwdziennik@gmail.com>
2026-04-16 19:17:31 -0400

    Fix parser package bootstrap imports for Streamlit Cloud

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,10 +1,10 @@
 """PDF parser modules for accounting exports."""
 
-from financial_validator_mvp.parsers.balance_sheet_parser import parse_balance_sheet_pdf, parse_balance_sheet_text
-from financial_validator_mvp.parsers.bank_statement_parser_pnc import parse_pnc_statement_pdf, parse_pnc_statement_text
-from financial_validator_mvp.parsers.document_classifier import build_validation_packs, classify_document, classify_document_text
-from financial_validator_mvp.parsers.general_ledger_parser import parse_general_ledger_pdf, parse_general_ledger_text
-from financial_validator_mvp.parsers.pnl_parser import parse_pnl_pdf, parse_pnl_text
+from .balance_sheet_parser import parse_balance_sheet_pdf, parse_balance_sheet_text
+from .bank_statement_parser_pnc import parse_pnc_statement_pdf, parse_pnc_statement_text
+from .document_classifier import build_validation_packs, classify_document, classify_document_text
+from .general_ledger_parser import parse_general_ledger_pdf, parse_general_ledger_text
+from .pnl_parser import parse_pnl_pdf, parse_pnl_text
 
 __all__ = [
     "build_validation_packs",
